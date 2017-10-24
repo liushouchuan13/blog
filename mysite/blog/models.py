718f9604235310ca3165f64f391e0d87f3dfd89d
liushouchuan13 <liushouchuan13@gmail.com>
2017-10-24 20:21:51 +0800

    two

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -29,6 +29,13 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime("%m"),
+                             self.publish.strftime("%d"),
+                             self.slug
+                       ])
 
 
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_objects_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'post': posts}
+    return render(request, 'blog/post/list.html', context})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', pulish__year='year', publish__month='month', publish__day='day')
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context})
